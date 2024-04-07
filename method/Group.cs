d4b7df50923426db9a2740853bef62b348914e11
sahib memmedov <sahibmemmedov7890@gmail.com>
2024-04-07 12:10:31 -0700

    initial commit

---
+++ method/Group.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._04._2024_2_.method
+{
+    internal class Group
+    {
+        public static Group[] Groups { get; set; }
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+    }
+}
---
+++ method/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._04._2024_2_.method
+{
+    internal class Student
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; } 
+        public string Surname { get; set; }
+
+        public Student(string name, string surname)
+        {
+            Id = Count++;
+            Name = name;
+            Surname = surname;
+        }
+
+        public string GetInfo()
+        {
+            return ($"Id: {Id}, Name: {Name}, Surname: {Surname}");
+        }
+    }
+}
+}
