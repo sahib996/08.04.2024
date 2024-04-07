b593002e3ea5b740bd443ee20e5a8816eeb5d980
sahib memmedov <sahibmemmedov7890@gmail.com>
2024-04-07 12:07:06 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08._04._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ method/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._04._2024.method
+{
+    internal  static class Helper
+    {
+        public static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
